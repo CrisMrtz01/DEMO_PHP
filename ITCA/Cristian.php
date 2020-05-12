fca47556e6e98fc26294824800dbd6b86816033a
Cristian Mrtz <kooper8273@gmail.com>
2020-05-12 10:45:27 -0600

    Creacion del directorio ITCA y ficheros PHP

---
+++ ITCA/Cristian.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yo</title>
+</head>
+<body>
+<h3>Al principio es realmente confuso que hayan tantos Git por decirlo asi,
+    pero creo que le voy tomando el ritmo con un poco de practica.
+</h3>
+    
+</body>
+</html>
---
+++ ITCA/Martinez.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yo</title>
+</head>
+<body>
+    <h3>
+        Aunque creo que es un poco complicado al principio y aun mas con la cantidad de contenido 
+        que se requiere, con paciencia y práctica se pueden lograr los objetivos planteados.   
+    </h3>
+</body>
+</html>
